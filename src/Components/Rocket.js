c4f70d719e3513c44066cbe381eb88cad676e0ee
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-09-28 15:38:57 +0200

    Add Reservation action

--- src/Components/Rocket.js
+++ src/Components/Rocket.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { fetchRockets, ChangeStatus } from '../Redux/Rockets/Rockets';
 
-const Rocket = ({ rocket }) => (
-  <div>
+const Rocket = ({ rocket }) => {
+  const dispatch = useDispatch();
+  const rocketHandleBtn = (id) => {
+    dispatch(ChangeStatus(id));
+  };
+  const rockets = useSelector((state) => state.rockets);
+  useEffect(() => {
+    if (!rockets.length) dispatch(fetchRockets());
+  }, []);
+
+  return (
     <div>
-      <div><img src={rocket.flickrImages[0]} alt="Rocket-Banner" /></div>
       <div>
+        <div><img src={rocket.flickrImages[0]} alt="Rocket-Banner" /></div>
         <div>
-          <h1>{rocket.name}</h1>
-          <p>{rocket.description}</p>
+          <div>
+            <h1>{rocket.name}</h1>
+            <p>{rocket.description}</p>
+          </div>
+          <div><button type="submit" onClick={() => rocketHandleBtn(rocket.id)}>Reserve Rocket</button></div>
         </div>
-        <div><button type="submit">Reserve Rocket</button></div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 Rocket.propTypes = {
   rocket: PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string,
     description: PropTypes.string,
-    flickrImages: PropTypes.string,
+    flickrImages: PropTypes.instanceOf(Array),
   }).isRequired,
 };
 
