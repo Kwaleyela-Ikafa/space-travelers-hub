776444393fd0538780636dcb085376cd2de06791
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-09-28 17:15:27 +0200

    Implement rocket booking actions

--- src/Components/Rocket.js
+++ src/Components/Rocket.js
@@ -5,7 +5,7 @@ import { fetchRockets, ChangeStatus } from '../Redux/Rockets/Rockets';
 
 const Rocket = ({ rocket }) => {
   const dispatch = useDispatch();
-  const rocketHandleBtn = (id) => {
+  const handleChangeBtn = (id) => {
     dispatch(ChangeStatus(id));
   };
   const rockets = useSelector((state) => state.rockets);
@@ -20,18 +20,45 @@ const Rocket = ({ rocket }) => {
         <div>
           <div>
             <h1>{rocket.name}</h1>
-            <p>{rocket.description}</p>
+            <div>
+              {rocket.reserved && (
+              <badge className="bg-blue-500 p-1 text-white rounded-sm pr-3 pl-3">Reserved</badge>
+              )}
+              <p>
+                {rocket.description}
+              </p>
+            </div>
+
           </div>
-          <div><button type="submit" onClick={() => rocketHandleBtn(rocket.id)}>Reserve Rocket</button></div>
+          <div>
+            {rocket.reserved
+              ? (
+                <button
+                  onClick={() => handleChangeBtn(rocket.id)}
+                  type="button"
+                >
+                  Cancel Reservation
+                </button>
+              ) : (
+                <button
+                  onClick={() => handleChangeBtn(rocket.id)}
+                  type="button"
+                >
+                  Reserve Rocket
+                </button>
+              )}
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
 Rocket.propTypes = {
   rocket: PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string,
+    reserved: PropTypes.bool,
     description: PropTypes.string,
     flickrImages: PropTypes.instanceOf(Array),
   }).isRequired,
