94217bf43baf8390754fce56040721917b8dcf7d
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-09-30 15:44:23 -0400

    resolve conflict with myprofile styling branch

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,41 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../asses/space-traveler-logo.png';
 
 const Header = () => (
-  <header>
-    <div>
-      <img src={logo} alt="logo" />
-      <h1>Space Travelers Hub</h1>
-    </div>
-    <nav>
-      <Link to="/">Rockets</Link>
-      <Link to="/Dragons">Dragons</Link>
-      <Link to="/Missions">Missions</Link>
-      <Link to="/MyProfile">MyProfile</Link>
-    </nav>
-  </header>
+  <>
+    <header>
+      <div>
+        <img src={logo} alt="logo" />
+        <h1>Space Travelers Hub</h1>
+      </div>
+      <nav>
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({
+            color: isActive ? '#fff' : '#545e6f',
+            background: isActive ? '#7600dc' : '#f0f0f0',
+          })}
+        >
+          Rockets
+
+        </NavLink>
+        <NavLink
+          to="/Dragons"
+          style={({ isActive }) => ({
+            color: isActive ? '#fff' : '#545e6f',
+            background: isActive ? '#7600dc' : '#f0f0f0',
+          })}
+        >
+          Dragons
+
+        </NavLink>
+        <NavLink to="/Missions">Missions</NavLink>
+        <NavLink to="/MyProfile">MyProfile</NavLink>
+      </nav>
+    </header>
+    <div className="Line" />
+
+  </>
 );
 
 export default Header;
