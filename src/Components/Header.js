4d407e04c41e41da53df0b26c8d72d34e0a5484c
Anas <anas.chaabane98@gmail.com>
2023-03-28 15:12:48 +0100

    enhance nav style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,37 +11,21 @@ const Header = () => (
       <nav>
         <NavLink
           to="/Rockets"
-          style={({ isActive }) => ({
-            color: isActive ? '#fff' : '#545e6f',
-            background: isActive ? '#7600dc' : '#f0f0f0',
-          })}
         >
           Rockets
         </NavLink>
         <NavLink
           to="/Dragons"
-          style={({ isActive }) => ({
-            color: isActive ? '#fff' : '#545e6f',
-            background: isActive ? '#7600dc' : '#f0f0f0',
-          })}
         >
           Dragons
         </NavLink>
         <NavLink
           to="/Missions"
-          style={({ isActive }) => ({
-            color: isActive ? '#fff' : '#545e6f',
-            background: isActive ? '#7600dc' : '#f0f0f0',
-          })}
         >
           Missions
         </NavLink>
         <NavLink
           to="/MyProfile"
-          style={({ isActive }) => ({
-            color: isActive ? '#fff' : '#545e6f',
-            background: isActive ? '#7600dc' : '#f0f0f0',
-          })}
         >
           MyProfile
         </NavLink>
